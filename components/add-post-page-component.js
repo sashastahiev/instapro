b87d92e665599945eeb2f38d4574baec40fa5d1b
sashastahiev <vebium@mail.ru>
2026-02-10 16:19:08 +0300

    commit

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -2,20 +2,41 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // @TODO: Реализовать страницу добавления поста
     const appHtml = `
-    <div class="page-container">
-      <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
-    </div>
-  `;
-
+      <div class="page-container">
+        <div class="header-container"></div>
+        <div class="form">
+          <h3 class="form-title">Добавить пост</h3>
+          <div class="form-inputs">
+            <textarea 
+              id="description-textarea" 
+              class="input textarea" 
+              placeholder="Описание фотографии"
+              rows="4"
+            ></textarea>
+            <div class="upload-image-container"></div>
+            <div class="form-error"></div>
+            <button class="button" id="add-button">Добавить</button>
+          </div>
+        </div>
+      </div>
+    `;
     appEl.innerHTML = appHtml;
 
+      // Обработчик кнопки "Добавить"
     document.getElementById("add-button").addEventListener("click", () => {
-      onAddPostClick({
-        description: "Описание картинки",
-        imageUrl: "https://image.png",
-      });
+      const description = document.getElementById("description-textarea").value.trim();
+      
+      if (!description) {
+        alert("Введите описание фотографии");
+        return;
+      }
+      
+      if (!imageUrl) {
+        alert("Не выбрана фотография");
+        return;
+      }
+      
+      onAddPostClick({ description, imageUrl });
     });
   };
 
